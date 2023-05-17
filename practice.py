09abe3a332bc5c429ea07a1a0670770ade67d096
AstridLicy <lichiyun@ut-vision.org>
2023-05-17 21:13:52 +0900

    problem1submission

--- practice.py
+++ practice.py
@@ -6,13 +6,10 @@ group3 = {'sara': 25, 'lisa': 36, 'anna':49, 'young':64, 'james':81, 'elizabeth'
 for info in group2:
     for key in info.keys():
         group1[key] = info[key]
-
-print(group1)
-print(group2)
-
+        
 # What will group1 and group2 become after executing this program? Please write them down in the following lines (without actually running this program).
-# group1: {'john': 1, 'doe': 2, 'tom': 9}
-# group2: [{'kevin': 4, 'tom': 9}, {jim: 16}]
+# group1: {'john': 1, 'doe': 2, 'tom': 9, 'Kevin': 4, 'jimmy': 16}
+# group2: [{'kevin': 4, 'tom': 9}, {'jimmy': 16}]
 
 # Then you want to write all the keys and the square root of their corresponding values in group3 into the 1-th dictionary of group2 (e.g. if there is a key ’tom’ whose value is 4, then you need to write a key ‘tom’ whose value is the square root of 4 into group2). Write your codes to achieve this. 
 # Write your codes below:
