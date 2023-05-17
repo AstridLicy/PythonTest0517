ebe406e8dc397a195943313ca88e9f47c89714f6
Yoyogi <kevincenty226@gmail.com>
2023-05-17 13:30:54 +0900

    minor fixes

--- practice.py
+++ practice.py
@@ -1,6 +1,7 @@
 jim = 'jimmy'
 group1 = {'john': 1, 'doe': 2, 'tom': 6}
 group2 = [{'kevin': 4, 'tom': 9}, {jim: 16}]
+group3 = {'sara': 25, 'lisa': 36, 'anna':49}
 
 for info in group2:
     for key in info.keys():
